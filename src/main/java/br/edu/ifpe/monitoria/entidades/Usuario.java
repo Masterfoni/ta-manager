4a09e5b79afdf87ac7bc69bebbab746ea287fde8
Felipe Lima <fal@a.recife.ifpe.edu.br>
2017-03-25 11:28:49 -0300

    [FAL-002] Modificacao de classe (Usuario) - a concluir

--- src/main/java/br/edu/ifpe/monitoria/entidades/Usuario.java
+++ src/main/java/br/edu/ifpe/monitoria/entidades/Usuario.java
@@ -2,6 +2,36 @@ package br.edu.ifpe.monitoria.entidades;
 
 import java.io.Serializable;
 
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+
+@Entity
+@SequenceGenerator (name = "SEQUENCIA_USUARIO",
+		sequenceName = "SQ_USUARIO",
+		initialValue = 1,
+		allocationSize = 1)
+@Table(name="TB_USUARIO")
+@Access(AccessType.FIELD)
 public class Usuario implements Serializable{
 
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.SEQUENCE, generator="SEQUENCIA_USUARIO")
+	private Long id;
+	
+	@Column (name="TXT_NOME")
+	private String nome;
+	@Column (name="TXT_EMAIL")
+	private String email;
+	
+	
 }
