51de4791fb8e72ea9ab04672402d0574971b62e4
Felipe Lima <fal@a.recife.ifpe.edu.br>
2017-04-01 19:38:29 -0300

    [FAL-001] Modificação entidade Coordenacao - PARA HOMOLOGAR

--- src/main/java/br/edu/ifpe/monitoria/entidades/Coordenacao.java
+++ src/main/java/br/edu/ifpe/monitoria/entidades/Coordenacao.java
@@ -2,14 +2,48 @@ package br.edu.ifpe.monitoria.entidades;
 
 import java.io.Serializable;
 
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@SequenceGenerator (name = "SEQUENCIA_COORDENACAO",
+		sequenceName = "SQ_COORDENACAO",
+		initialValue = 1,
+		allocationSize = 1)
+@Table(name="TB_COORDENACAO")
+@Access(AccessType.FIELD)
 public class Coordenacao implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue (strategy = GenerationType.SEQUENCE, generator="SEQUENCIA_DEPARTAMENTO")
 	private Long id;
+	
+	@Column (name="TXT_NOME")
 	private String nome;
+	
+	@Column (name="TXT_SIGLA")
 	private String sigla;
+	
+	@ManyToOne (fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "ID_DEPARTAMENTO", referencedColumnName = "ID")
 	private Departamento departamento;
-	private Usuario coordenador;
+	
+	@OneToOne (fetch = FetchType.LAZY, cascade = CascadeType.ALL, optional = false)
+	@JoinColumn(name = "ID_COORDENADOR", referencedColumnName = "ID")
+	private Professor coordenador;
 	
 }
