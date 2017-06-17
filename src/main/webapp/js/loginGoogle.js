5fcd271a81802998e03ef7149318a96d87de7f91
Felipe Lima <fal@a.recife.ifpe.edu.br>
2017-06-17 18:00:01 -0300

    [FAL] Finalização da integração no index do login do google, personalização do botao de login do google, alteração de design do index

--- src/main/webapp/js/loginGoogle.js
+++ src/main/webapp/js/loginGoogle.js
@@ -1,11 +1,48 @@
 /**
  * 
  */
+
+jQuery(function(){
+	jQuery.noConflict();
+});
+
+onload = startApp;
+
+var googleUser = {};
+
+//var startApp = function() {
+function startApp(){
+  gapi.load('auth2', function(){
+    // Retrieve the singleton for the GoogleAuth library and set up the client.
+    auth2 = gapi.auth2.init({
+      client_id: '835716531913-m3mt3905k8itbmflg8t7mlqabgjcruce.apps.googleusercontent.com',
+      cookiepolicy: 'single_host_origin',
+      // Request scopes in addition to 'profile' and 'email'
+      //scope: 'additional_scope'
+    });
+    attachSignin(document.getElementById('customBtn'));
+  });
+};
+
+function attachSignin(element) {
+  console.log(element.id);
+  auth2.attachClickHandler(element, {},
+      function(googleUser) {
+        document.getElementById('name').innerText = "Signed in: " +
+            googleUser.getBasicProfile().getName();
+        onSignIn(googleUser);
+      }, 
+      function(error) {
+        alert(JSON.stringify(error, undefined, 2));
+      });
+}
+
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
       console.log('User signed out.');
     });
+    window.location.href = "index.xhtml";
 }
 
 function onSignIn(googleUser) {
@@ -24,7 +61,5 @@ function onSignIn(googleUser) {
 
 function handleComplete(xhr, status, args) {
     var nomeDoAtributo = args.logou;
-
-    // Atualizar UI
     alert(nomeDoAtributo);
 }
