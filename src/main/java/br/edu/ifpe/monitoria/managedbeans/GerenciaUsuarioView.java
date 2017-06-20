698e77c3eab4307d0c1b1ce4174c36aa012cf03e
Masterfoni <joaovitor_179@hotmail.com>
2017-06-20 18:26:43 -0300

    [FEATURE] Adição de página para manipulação de usuários, servirá como referência para outros CRUD's. Remoção de lixo e criação de menu hamburguer na navbar

---
+++ src/main/java/br/edu/ifpe/monitoria/managedbeans/GerenciaUsuarioView.java
@@ -0,0 +1,37 @@
+package br.edu.ifpe.monitoria.managedbeans;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+
+import br.edu.ifpe.monitoria.entidades.Usuario;
+import br.edu.ifpe.monitoria.localbean.UsuarioLocalBean;
+
+@ManagedBean (name="gerenciaUsuarioView")
+public class GerenciaUsuarioView implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@EJB
+	private UsuarioLocalBean usuariobean;
+
+	public List<Usuario> usuarios;
+	
+	public List<Usuario> getUsuarios() {
+		return usuarios;
+	}
+
+	public void setUsuarios(List<Usuario> usuarios) {
+		this.usuarios = usuarios;
+	}
+
+	public GerenciaUsuarioView() {
+	}
+	
+	public List<Usuario> listagemUsuarios() {
+		usuarios = usuariobean.consultaUsuarios();
+		return usuarios;
+	}
+}
--- src/main/java/br/edu/ifpe/monitoria/managedbeans/HomePageView.java
+++ src/main/java/br/edu/ifpe/monitoria/managedbeans/HomePageView.java
@@ -1,8 +1,10 @@
 package br.edu.ifpe.monitoria.managedbeans;
 
+import java.io.IOException;
 import java.io.Serializable;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 
 @ManagedBean (name="homePageView")
 public class HomePageView implements Serializable {
@@ -11,5 +13,14 @@ public class HomePageView implements Serializable {
 
 	public HomePageView() {
 		
+	}
+	
+	public String gotoGerUsuarios() {
+		return "gerenciaUsuario";
+		//		try {
+//			FacesContext.getCurrentInstance().getExternalContext().dispatch("/gerenciaUsuario.xhtml");
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
 	}
 }
