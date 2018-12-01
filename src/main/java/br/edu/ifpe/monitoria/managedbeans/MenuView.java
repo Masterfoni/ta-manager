d9d984cf58fc14570601af5042ef3d4d5f7633dc
Masterfoni <joaovitor_179@hotmail.com>
2018-12-01 10:38:58 -0300

    fix(aluno): Correção de acessos do menu para alunos que são monitores

--- src/main/java/br/edu/ifpe/monitoria/managedbeans/MenuView.java
+++ src/main/java/br/edu/ifpe/monitoria/managedbeans/MenuView.java
@@ -3,9 +3,14 @@ package br.edu.ifpe.monitoria.managedbeans;
 import java.io.Serializable;
 
 import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+import br.edu.ifpe.monitoria.localbean.AlunoLocalBean;
+import br.edu.ifpe.monitoria.localbean.MonitoriaLocalBean;
 
 @ManagedBean (name="menuView")
 @SessionScoped
@@ -13,17 +18,33 @@ public class MenuView implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@EJB
+	MonitoriaLocalBean monitoriaBean;
+	
+	@EJB
+	AlunoLocalBean alunoBean;
+	
 	boolean comissao;
 	
 	boolean aluno;
 	
 	boolean professor;
+	
+	boolean monitor;
 
 	@PostConstruct
 	public void init() {
+		HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+		
 		comissao = FacesContext.getCurrentInstance().getExternalContext().isUserInRole("comissao");
 		aluno = FacesContext.getCurrentInstance().getExternalContext().isUserInRole("aluno");
 		professor = FacesContext.getCurrentInstance().getExternalContext().isUserInRole("professor");
+		
+		if (aluno) {
+			monitor = monitoriaBean.isCurrentMonitor(alunoBean.consultaAlunoById((Long)session.getAttribute("id")));
+		} else {
+			monitor = false;
+		}
 	}
 
 	public boolean isComissao() {
@@ -48,5 +69,13 @@ public class MenuView implements Serializable {
 
 	public void setProfessor(boolean professor) {
 		this.professor = professor;
+	}
+
+	public boolean isMonitor() {
+		return monitor;
+	}
+
+	public void setMonitor(boolean monitor) {
+		this.monitor = monitor;
 	}
 }
