d88af9602ae8d86b319f3583676151b9314b7ab1
Felipe Lima <fal@a.recife.ifpe.edu.br>
2019-04-02 20:12:04 -0300

    fix teste cucumber candidatura

--- src/test/java/br/edu/ifpe/monitoria/step_definitions/CandidaturaSteps.java
+++ src/test/java/br/edu/ifpe/monitoria/step_definitions/CandidaturaSteps.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.Select;
 
 import br.edu.ifpe.monitoria.testutils.BrowserManager;
 import br.edu.ifpe.monitoria.testutils.Dataset;
@@ -25,7 +24,7 @@ public class CandidaturaSteps {
 	
 	@Dado("^que o usuario esta logado como aluno$")
 	public void queOUsuarioEstaLogadoComoAluno() throws Throwable {
-	    LoginSteps.logar("joaovitor8891879@gmail.com", "");
+	    LoginSteps.logar("gemteste1@gmail.com", "");
 	}
 
 	@Dado("^esteja na pagina de inscricao$")
@@ -35,25 +34,18 @@ public class CandidaturaSteps {
 
 	@Dado("^se inscrever para monitoria em um componente curricular$")
 	public void seInscreverParaMonitoriaEmUmComponenteCurricular() throws Throwable {
-		Select edital = new Select(BrowserManager.driver.findElement(By.id("escolhaedital:selectEdital")));
-		edital.selectByValue("1");
-		BrowserManager.driver.findElement(By.id("escolhaedital:selecionarEdital")).click();
-		
-		Select curso = new Select(BrowserManager.driver.findElement(By.id("escolhacurso:selectCurso")));
-		curso.selectByValue("1");
-		BrowserManager.driver.findElement(By.id("escolhacurso:selecionarCurso")).click();
-		
-		BrowserManager.driver.findElement(By.id("escolhaplano:repeatPlanos:0:btnBolsista")).click();
-		
+		BrowserManager.driver.findElement(By.id("escolhaplano:repeatPlanos:0:botaoBolsista")).click();
 	}
 
 	@Entao("^o aluno se candidata a monitoria$")
 	public void oAlunoSeCandidataAMonitoria() throws Throwable {
 		WebElement inscrito = BrowserManager.driver.findElement(By.id("inscrito"));
 		List <WebElement> texto = inscrito.findElements(By.tagName("p"));
-		assertEquals("Componente curricular: Trabalho de Conclusão de Curso", texto.get(0).getText());
-		assertEquals("Turno: NOTURNO", texto.get(1).getText());
-		assertEquals("Bolsista: Sim", texto.get(2).getText());
+		assertEquals("Inscrição atual", texto.get(0).getText());
+		
+		assertEquals("Componente curricular: Trabalho de Conclusão de Curso\n" + 
+				"Turno: NOTURNO\n" + 
+				"Modalidade: Bolsista", texto.get(1).getText());
 	}
 	
 }
